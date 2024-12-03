1d3640c7275aaf0a129f0baa31f334de6f885122
abreulud <ludmillaabreu07@gmail.com>
2024-12-02 22:21:44 -0300

    pag inicial

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -23,27 +23,12 @@ export const HomePage: React.FC = () => {
             </h1>
 
             <h2>
-              Body text for your whole article or post. We’ll put in some
-              <br />
-              lorem ipsum to show how a filled
-              <br />
-              -out page might look:
+            Os impactos do descarte incorreto de resíduos no meio ambiente são alarmantes, afetando solos, rios, oceanos e agravando a crise climática. 
+             
             </h2>
             <p className="homePageSubtitle">
-              Excepteur efficient emerging, minim veniam anim aute carefully
-              curate
-              <br />
-              Ginza conversationexquisite perfect nostrud nisi intricate
-              Content. Qui
-              <br />
-              international first-class nulla ut. adipisicing, essential lovely
-              queen
-              <br />
-              tempor eiusmod irure. Exclusive izakaya charming
-              <br />
-              Scandinavian impeccable aute quality of life soft power pariatur
-              <br />
-              Melbourne occaecat
+            A Plataforma ESC busca conscientizar sobre esses problemas, oferecendo informações e ferramentas para práticas sustentáveis de forma interativa. Alinhada ao ODS 12, nossa missão é contribuir para o consumo e produção responsáveis, promovendo um futuro mais sustentável. 
+            Aprenda e transforme conosco!
             </p>
           </div>
           <img alt="Main Image" src={mainImage} />
