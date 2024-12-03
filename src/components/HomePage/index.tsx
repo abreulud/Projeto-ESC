9f4687ba0804210e9070c5d3ff90e011994f1456
ibraDajani <ibrahimdajani121@gmail.com>
2024-12-03 00:56:28 -0300

    adding api percentages on home screen

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -3,8 +3,23 @@ import React, { useEffect } from 'react';
 import mainImage from '../../assets/images/main_image.png';
 import imagePage from '../../assets/images/page.png';
 import { api } from '../../services/api';
+import CO2 from '../../assets/elements/co2.png';
+import NH3 from '../../assets/elements/nh3.png';
+import NO2 from '../../assets/elements/no2.png';
+import O3 from '../../assets/elements/o3.png';
+import PM25 from '../../assets/elements/pm25.png';
+import SO2 from '../../assets/elements/so2.png';
 
 const API_KEY = '032a8fa50ef0f9979a5439c779976b7d';
+
+const elementIcons = {
+  co: CO2,
+  nh3: NH3,
+  no2: NO2,
+  o3: O3,
+  pm25: PM25,
+  so2: SO2,
+};
 
 export const HomePage: React.FC = () => {
   const [apiData, setApiData] = React.useState<any>(null);
@@ -21,23 +36,35 @@ export const HomePage: React.FC = () => {
       .then(response => {
         const components = response.data.list[0].components;
 
-        const total = Object.values(components).reduce(
+        const filteredComponents = ['nh3', 'no2', 'o3', 'co'].reduce(
+          (acc, key) => {
+            if (components[key] !== undefined) {
+              acc[key] = components[key];
+            }
+            return acc;
+          },
+          {},
+        );
+
+        const total = Object.values(filteredComponents).reduce(
           (sum, value) => sum + value,
           0,
         );
 
-        const componentArray = Object.entries(components).map(
+        const componentArray = Object.entries(filteredComponents).map(
           ([key, value]) => ({
             name: key,
             percentage: ((value / total) * 100).toFixed(2),
+            icon: elementIcons[key],
           }),
         );
+
         setApiData(componentArray);
       });
   };
 
   useEffect(() => {
-    getApiData({ lat: '35', lon: '139' });
+    getApiData({ lat: '-12.908859', lon: '-38.455125' });
   }, []);
 
   return (
@@ -74,18 +101,40 @@ export const HomePage: React.FC = () => {
           </div>
           <img alt="Main Image" src={mainImage} />
         </section>
-        
       </div>
       <img className="imageMainPage" alt="Image Page" src={imagePage} />
-      <div>
-      {apiData?.length
-          ? apiData?.map((component: any) => (
-              <div key={component.name}>
-                <p>{component.name}</p>
-                <p>{component.percentage}</p>
-              </div>
-            ))
-          : null}
+      <div
+        style={{
+          backgroundColor: '#fffcee',
+        }}>
+        <h1 style={{ textAlign: 'center' }}>
+          Impacto dos{' '}
+          <span style={{ color: '#457461' }}>
+            gases poluentes
+            <br />
+          </span>
+        </h1>
+        <div
+          style={{
+            flexDirection: 'row',
+            display: 'flex',
+            gap: 50,
+            alignItems: 'center',
+            justifyContent: 'space-evenly',
+          }}>
+          {apiData?.length
+            ? apiData?.map((component: any) => (
+                <div key={component.name}>
+                  <img
+                    src={elementIcons[component.name]}
+                    alt={component.name}
+                    style={{ height: 50, width: 50 }}
+                  />
+                  <p style={{color: 'black', fontWeight: 'bold', fontSize: 18}}>{component.percentage}%</p>
+                </div>
+              ))
+            : null}
+        </div>
       </div>
     </>
   );
