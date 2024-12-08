2d78328f22dbef367a537151164d0a8db574eb7b
abreulud <ludmillaabreu07@gmail.com>
2024-12-08 01:22:59 -0300

    pagina inicial continuação

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,7 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect } from 'react';
-import mainImage from '../../assets/images/main_image.png';
-import imagePage from '../../assets/images/page.png';
+import React, { useEffect, useState } from "react";
+import mainImage from "../../assets/images/main_image.png";
+import imagePage from "../../assets/images/page.png";
+import dashboard from "../../assets/images/dashboard.png";
+import city from "../../assets/images/city.png";
+import tree from "../../assets/images/tree.png";
+
+
 import { api } from '../../services/api';
 import CO2 from '../../assets/elements/co2.png';
 import NH3 from '../../assets/elements/nh3.png';
@@ -22,7 +26,7 @@ const elementIcons = {
 };
 
 export const HomePage: React.FC = () => {
-  const [apiData, setApiData] = React.useState<any>(null);
+  const [apiData, setApiData] = useState<any>(null);
 
   const getApiData = async ({ lat, lon }: { lat: string; lon: string }) => {
     await api
@@ -66,46 +70,60 @@ export const HomePage: React.FC = () => {
   useEffect(() => {
     getApiData({ lat: '-12.908859', lon: '-38.455125' });
   }, []);
-
+  
   return (
-    <>
+    <div style={{backgroundColor: '#FFFCEE', display: "flex", flexDirection: "column", paddingTop:140}}>
       <div className="container">
         <section className="mainContent">
           <div>
             <h1>
-              <span style={{ color: '#457461' }}>E</span>
-              <span style={{ color: 'black' }}>
+              <span style={{ color: "#457461" }}>E</span>
+              <span style={{ color: "black" }}>
                 ducação,
                 <br />
               </span>
-              <span style={{ color: '#43AD7C' }}>S</span>
-              <span style={{ color: 'black' }}>
+              <span style={{ color: "#43AD7C" }}>S</span>
+              <span style={{ color: "black" }}>
                 ustentabilidade e<br />
               </span>
-              <span style={{ color: '#457461' }}>C</span>
-              <span style={{ color: 'black' }}>onexão</span>
+              <span style={{ color: "#457461" }}>C</span>
+              <span style={{ color: "black" }}>onexão</span>
             </h1>
 
             <h2>
-              Os impactos do descarte incorreto de resíduos no meio ambiente são
-              alarmantes, afetando solos, rios, oceanos e agravando a crise
-              climática.
+              Os impactos do descarte incorreto de resíduos no meio ambiente são alarmantes, afetando solos, rios, oceanos e agravando a crise climática.
+
             </h2>
             <p className="homePageSubtitle">
-              A Plataforma ESC busca conscientizar sobre esses problemas,
-              oferecendo informações e ferramentas para práticas sustentáveis de
-              forma interativa. Alinhada ao ODS 12, nossa missão é contribuir
-              para o consumo e produção responsáveis, promovendo um futuro mais
-              sustentável. Aprenda e transforme conosco!
+              A Plataforma ESC busca conscientizar sobre esses problemas, oferecendo informações e ferramentas para práticas sustentáveis de forma interativa. Alinhada ao ODS 12, nossa missão é contribuir para o consumo e produção responsáveis, promovendo um futuro mais sustentável.
+              Aprenda e transforme conosco!
             </p>
+
           </div>
           <img alt="Main Image" src={mainImage} />
         </section>
       </div>
-      <img className="imageMainPage" alt="Image Page" src={imagePage} />
+      <div style={{
+        backgroundColor: "#386050", paddingTop:35, marginTop:140, paddingBottom:30, borderRadius: 35, display: "flex",
+        flexDirection: "column", justifyContent: "center", alignItems: "center"
+      }}>
+        <h2 style={{ color: "#D9EFDE", fontSize: 28, textAlign: "center" }}>
+          “Juntos, podemos reduzir a poluição do ar e manter as pessoas e o planeta saudáveis ​​e seguros.” - ONU
+        </h2>
+        <img className="imageMainPage" alt="Image Page" src={imagePage} />
+      </div>
+
+      <div style={{
+        flexDirection: "column", justifyContent: "center", alignItems: "center", display: "flex", paddingTop:150, 
+        backgroundColor: "#FFFCEE" 
+      }}> 
+        <img className="dashboard" alt="dashboard" src={dashboard}/>
+        
+      </div>
+      
       <div
         style={{
-          backgroundColor: '#fffcee',
+          backgroundColor: '#fffcee', paddingTop: 120
         }}>
         <h1 style={{ textAlign: 'center' }}>
           Impacto dos{' '}
@@ -118,9 +136,9 @@ export const HomePage: React.FC = () => {
           style={{
             flexDirection: 'row',
             display: 'flex',
-            gap: 50,
             alignItems: 'center',
             justifyContent: 'space-evenly',
+            paddingTop: 55
           }}>
           {apiData?.length
             ? apiData?.map((component: any) => (
@@ -128,14 +146,33 @@ export const HomePage: React.FC = () => {
                   <img
                     src={elementIcons[component.name]}
                     alt={component.name}
-                    style={{ height: 50, width: 50 }}
+                    style={{ height: 100, width: 100 }}
                   />
-                  <p style={{color: 'black', fontWeight: 'bold', fontSize: 18}}>{component.percentage}%</p>
+                  <p style={{color: 'black', fontWeight: 'bold', fontSize: 18, textAlign:"center"}}>{component.percentage}%</p>
                 </div>
               ))
             : null}
         </div>
       </div>
-    </>
+      
+      <div style={{
+        flexDirection: "column", justifyContent: "center", alignItems: "center", display: "flex", paddingTop: 130, 
+        backgroundColor: "#FFFCEE" 
+      }}> 
+        <img className="city" alt="city" src={city}/>
+
+      </div>
+
+      <div style={{display:"flex", flexDirection:"column", justifyContent:"center", 
+        textAlign:"center", color: '#FFFCEE', position: "relative"
+      }}>
+      <img className="tree" alt="tree" src={tree}/>
+      <p className="treeText">
+      "A natureza saudável é um direito<br/>humano. Hoje, a poluição do ar<br/>nega a milhares de milhões de<br/>pessoas os seus direitos.<br/>O ar poluído afeta 99% das<br/> pessoas no planeta"<br/>- Organização das Nações Unidas (ONU)
+      </p>
+      </div>
+
+
+    </div>
   );
 };
